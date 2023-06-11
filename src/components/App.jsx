de62a20f9112298dbdcc453b71b8d1013fd89b49
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-06-11 19:16:45 +0200

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 16,
+        color: '#010101'
+      }}
+    >
+    </div>
+  );
+};
