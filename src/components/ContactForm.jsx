ab4d12569819750743b090295d94dcfba8bea046
Kamil Trzmiel <k.trzmiel@gmail.com>
2023-06-16 22:39:40 +0200

    feature pattern in ContactForm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -29,12 +29,18 @@ class ContactForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <input type="text"
           name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
           placeholder="Name"
           value={name}
           onChange={this.handleChange}
         />
         <input type="tel"
           name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
           placeholder="Number"
           value={number}
           onChange={this.handleChange}
